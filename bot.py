f61501dfb597e5b36a582960866b2cac77146290
Aleksandr Fonov <FonovAD@Gmail.com>
2023-02-03 19:29:18 +0300

    Added list theory. The names of some photos have been changed. The path to the book has been changed. The teams have been renamed.

--- bot.py
+++ bot.py
@@ -13,15 +13,22 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message):
     a = f'Hello, {message.from_user.first_name}\n'
-    mess = a + '/get_book' + '/системы_счисления' + '/int' + '/float' + '/string' + '/control_characters'
+    mess = (a 
+    + '/get_book\n'
+    + '/системы_счисления\n'
+    + '/int\n'
+    + '/float\n'
+    + '/string\n'
+    + '/управляющие_символы\n'
+    + '/списки')
     bot.send_message(message.chat.id, mess, parse_mode='html')
     
     
 @bot.message_handler(commands=['get_book'])
 def get_book(message):
-    doc = open('/Users/aleksandr/Desktop/Prostoy_Pyton.pdf', 'rb')
+    bot.send_message(message.chat.id, 'please wait...', parse_mode='html')
+    doc = open('/Users/aleksandr/Documents/Programming/Python/telegran_python_guide_bot/Prostoy_Pyton.pdf', 'rb')
     bot.send_document(message.chat.id, doc)
-    bot.send_document(message.chat.id, "FILEID")
     doc.close()
     
     
@@ -35,13 +42,13 @@ def sist(message):
     
 @bot.message_handler(commands=['int'])
 def _int_(message):
-    photo = open('/Users/aleksandr/Documents/Programming/Python/telegran_python_guide_bot/int/Без имени-1.png', 'rb')
+    photo = open('/Users/aleksandr/Documents/Programming/Python/telegran_python_guide_bot/int/picture.png', 'rb')
     bot.send_photo(message.chat.id, photo)
 
     
 @bot.message_handler(commands=['float'])
 def _float_(message):
-    photo = open('/Users/aleksandr/Documents/Programming/Python/telegran_python_guide_bot/float/Без имени-1.png', 'rb')
+    photo = open('/Users/aleksandr/Documents/Programming/Python/telegran_python_guide_bot/float/Picture.png', 'rb')
     bot.send_photo(message.chat.id, photo)
     
     
@@ -69,16 +76,29 @@ def _str_(message):
     
 @bot.message_handler(commands=['управляющие_символы'])
 def contr(message):
-    photo = open('/Users/aleksandr/Documents/Programming/Python/telegran_python_guide_bot/control_symb/Без имени-1.png', 'rb')
+    photo = open('/Users/aleksandr/Documents/Programming/Python/telegran_python_guide_bot/control_symb/Picture.png', 'rb')
     bot.send_photo(message.chat.id, photo)
     photo.close()
     
     
 @bot.message_handler(commands=['списки'])
 def contr(message):
-    photo = open('', 'rb')
-    bot.send_photo(message.chat.id, photo)
-    
-    
+    photo1 = open('/Users/aleksandr/Documents/Programming/Python/telegran_python_guide_bot/Lists/Pic1.png', 'rb')
+    photo2 = open('/Users/aleksandr/Documents/Programming/Python/telegran_python_guide_bot/Lists/Pic2.png', 'rb')
+    photo3 = open('/Users/aleksandr/Documents/Programming/Python/telegran_python_guide_bot/Lists/Pic3.png', 'rb')
+    photo4 = open('/Users/aleksandr/Documents/Programming/Python/telegran_python_guide_bot/Lists/pic4.png', 'rb')
+    msg = '''
+    Списки служат для того, чтобы хранить объекты в определенном порядке, особенно если порядок или содержимое могут изменяться.'''
+    bot.send_message(message.chat.id, msg, parse_mode='html')
+    bot.send_photo(message.chat.id, photo1)
+    bot.send_photo(message.chat.id, photo2)
+    bot.send_photo(message.chat.id, photo3)
+    bot.send_photo(message.chat.id, photo4)
+    photo4.close()
+    photo3.close()
+    photo2.close()
+    photo1.close()
+
+
     
 bot.polling(non_stop=True)
